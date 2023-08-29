4f60a151e76994ec715aa1bebd8aa7e68f21aa09
Schaeffer2013 <r-sschaeffer@live.com>
2023-08-29 09:40:04 -0400

    id

--- movie_requests.py
+++ movie_requests.py
@@ -2,3 +2,30 @@ import requests
 from keys import api_key
 import csv
 import re
+
+
+res = requests.get(f"http://www.omdbapi.com/?&apikey={api_key}")
+data = res.json()
+id = data['imdbID']
+title = str(data['Title'])
+time = int(data['Runtime'])
+genre = str(data['Genre'])
+wins = int(data['Awards'])
+noms = int(data['Awards'])
+office = int(data['BoxOffice'])
+res = requests.get(f"http://www.omdbapi.com?i={id}&apikey={api_key}")
+print(data)
+
+
+headers = ['Movie Title', 'Runtime', 'Genre', 'Award Wins', 'Award Nominations', 'Box Office']
+info = ['Movie Title', 'Runtime', 'Genre', 'Award Wins', 'Award Nominations', 'Box Office']
+
+def movie_res():
+    with open('oscar_winners.csv', 'a') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(headers)
+            writer.writerow(info)
+
+  
+
+
