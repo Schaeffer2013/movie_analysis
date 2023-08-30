167871651a72e592f9ab1199b0db8ab82134e8e8
Schaeffer2013 <r-sschaeffer@live.com>
2023-08-30 15:46:59 -0400

    updated code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Presentation link:
+In this unit 8 project a analysis was done to show comparisons of 15 movies and their box office earnings, awards won, nominations won, and how many of each genre movies were considered.
+# Presentation link: 
--- movie_requests.py
+++ movie_requests.py
@@ -14,29 +14,29 @@ def movie_res():
                 writer = csv.writer(f)
                 writer.writerow(headers)
 
-            for row in rows:
-                csv_id = row['IMDB']
-                res = requests.get(f"http://www.omdbapi.com?apikey={api_key}&i={csv_id}")
-                api_data = res.json() 
-                title = str(api_data['Title'])
-                time = int(api_data['Runtime'].split(" ")[0])
-                genre = str(api_data['Genre'].split(", "))
-                awards = re.findall(r'\d+', api_data['Awards'])
-                wins = int(awards[0]) if awards else 0
-                noms = int(awards[1]) if len(awards) > 1 else 0
-                office = int(api_data['BoxOffice'].strip("$").replace(",", ""))
-                rated = str(api_data['Rated'])
-                lang = str(api_data['Language'].split(", "))
-                director = str(api_data['Director'])
-               
+                for row in rows:
+                    csv_id = row['IMDB']
+                    res = requests.get(f"http://www.omdbapi.com?apikey={api_key}&i={csv_id}")
+                    api_data = res.json() 
+                    title = str(api_data['Title'])
+                    time = int(api_data['Runtime'].split(" ")[0])
+                    genre = str(api_data['Genre'].split(", "))
+                    awards = re.findall(r'\d+', api_data['Awards'])
+                    wins = int(awards[0]) if awards else 0
+                    noms = int(awards[1]) if len(awards) > 1 else 0
+                    office = int(api_data['BoxOffice'].strip("$").replace(",", ""))
+                    rated = str(api_data['Rated'])
+                    lang = str(api_data['Language'].split(", "))
+                    director = str(api_data['Director'])
+                
 
 
-            
-                info = [title, time, genre, wins, noms, office, rated, lang, director]
-                with open('movies.csv', 'w', newline='', encoding='utf-8') as f:
-                    writer = csv.writer(f)
-                    writer.writerow(headers)
-                    writer.writerow(info)
+                
+                    info = [title, time, genre, wins, noms, office, rated, lang, director]
+                    with open('movies.csv', 'w', newline='', encoding='utf-8') as f:
+                        writer.writerow(info)
+                    print(api_data)
+                    
                 
                 
 
