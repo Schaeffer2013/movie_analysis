b5c14688c47a60adb0425e275f951c17bc1a0c4c
Schaeffer2013 <r-sschaeffer@live.com>
2023-08-30 12:46:07 -0400

    graphing

--- movie_requests.py
+++ movie_requests.py
@@ -19,12 +19,12 @@ def movie_res():
                 res = requests.get(f"http://www.omdbapi.com?apikey={api_key}&i={csv_id}")
                 api_data = res.json() 
                 title = str(api_data['Title'])
-                time = int(api_data['Runtime'])
+                time = int(api_data['Runtime'].split(" ")[0])
                 genre = str(api_data['Genre'].split(", "))
                 awards = re.findall(r'\d+', api_data['Awards'])
                 wins = int(awards[0]) if awards else 0
                 noms = int(awards[1]) if len(awards) > 1 else 0
-                office = int(api_data['BoxOffice', 0])
+                office = int(api_data['BoxOffice'].strip("$").replace(",", ""))
                 rated = str(api_data['Rated'])
                 lang = str(api_data['Language'].split(", "))
                 director = str(api_data['Director'])
@@ -33,6 +33,10 @@ def movie_res():
 
             
                 info = [title, time, genre, wins, noms, office, rated, lang, director]
+                with open('movies.csv', 'w', newline='', encoding='utf-8') as f:
+                    writer = csv.writer(f)
+                    writer.writerow(headers)
+                    writer.writerow(info)
                 
                 
 
